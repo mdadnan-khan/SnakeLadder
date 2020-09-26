411e7b29a7f291aab30df07c88f672a00b0927d2
MD ADNAN KHAN <adnan1990877@gmail.com>
2020-09-27 02:52:29 +0530

    [Adnan]REFACTORE: java code to display initial position of player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,5 +4,7 @@ import java.util.*;
 public class SnakeLadder{
 	public static void main(String[] args){
 		System.out.println("Welcome to the snake and ladder game");
+		int position = 0;
+		System.out.println("Your initial position is : " + position);
 	}
 }
