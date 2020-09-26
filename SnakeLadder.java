fd08b086a13197a36f2c39890023390d4d5eaf6b
MD ADNAN KHAN <adnan1990877@gmail.com>
2020-09-27 03:07:25 +0530

    [Adnan]REFACTORED: java code to get number after dice roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,5 +6,8 @@ public class SnakeLadder{
 		System.out.println("Welcome to the snake and ladder game");
 		int position = 0;
 		System.out.println("Your initial position is : " + position);
+		int roll = (int) (Math.random() * 6) + 1;
+		
+		System.out.println("Dice roll number: " + roll);	
 	}
 }
