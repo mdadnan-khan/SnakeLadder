bf3bcd310f18b56821bbfb9416f6e9ebe14c2c00
MD ADNAN KHAN <adnan1990877@gmail.com>
2020-09-27 03:30:34 +0530

    [Adnan]REFACTORED: java code to repeat rolling the dice until the player reaches 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -13,25 +13,29 @@ public class SnakeLadder{
 		int position = 0;
 		System.out.println("Your initial position is : " + position);		
 		
-		int roll = (int) (Math.random() * 6) + 1;
-		System.out.println("Dice roll number: " + roll);
-
-		int option = (int) (Math.random() * 3);
+		while(position<100){
+			int roll = (int) (Math.random() * 6) + 1;
+			System.out.println("Dice roll number: " + roll);
 		
-		switch(option){
-			case SNAKE:
-				position = (position > roll) ? position - roll : 0;
-				System.out.println("Oops you got bit by a snake");
-				break;
-			case LADDER:
-				position  = (position+roll<100) ? position + roll : 100;
-				System.out.println("You got a ladder and your position increased by " + roll + " steps");
-				break;
-			case NO_PLAY:
-				System.out.println("Sorry!! No play");
-				break;
+			int option = (int) (Math.random() * 3);
+		
+			switch(option){
+				case SNAKE:
+					position = (position > roll) ? position - roll : 0;
+					System.out.println("Oops you got bit by a snake");
+					break;
+				case LADDER:
+					position  = (position+roll<=100) ? position + roll : 100;
+					System.out.println("You got a ladder and your position increased by " + roll + " steps");
+					break;
+				case NO_PLAY:
+					System.out.println("Sorry!! No play");
+					break;
+			}
+			System.out.println("Your current position is: " + position);
 		}
-		System.out.println("Your current position is: " + position);
+
+		System.out.println("Congratulations You won the game");
 
 	}
 	
